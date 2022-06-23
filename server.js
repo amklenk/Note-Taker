91df1837b6db97de11811dbd34ea693903f45b32
Amanda Klenk <amandamklenk3@gmail.com>
2022-06-23 14:11:30 -0400

    set up gets for html files along with static middleware

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 //requires
 const express = require('express');
-const data = require('./db/db.json');
+const notes = require('./db/db.json');
 const fs = require('fs');
 const path = require('path');
 
@@ -14,9 +14,10 @@ const uuid = require('./helpers/uuid');
 //middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static('public'));
 
-function filterByQuery(query, data) {
-    let filteredResults = data;
+function filterByQuery(query, notes) {
+    let filteredResults = notes;
     if (query.title) {
       filteredResults = filteredResults.filter(note => note.title === query.title);
     }
@@ -26,26 +27,26 @@ function filterByQuery(query, data) {
     return filteredResults;
   }
 
-app.get('/api/data', (req, res) => {
-    let results = data;
+app.get('/api/notes', (req, res) => {
+    let results = notes;
     if(req.query){
         results = filterByQuery(req.query, results);
     }
     res.json(results);
   });
 
-function findById(id, data) {
-    const result = data.filter(note => note.id === id)[0];
+function findById(id, notes) {
+    const result = notes.filter(note => note.id === id)[0];
     return result;
   }
 
-function createNewNote(body, data) {
+function createNewNote(body, notes) {
     const note = body;
-    data.push(note);
+    notes.push(note);
 
     fs.writeFileSync(
         path.join(__dirname, './db/db.json'),
-        JSON.stringify(data, null, 2)
+        JSON.stringify(notes, null, 2)
       );
 
   return note;
@@ -61,8 +62,8 @@ function createNewNote(body, data) {
     return true;
   }
 
-app.get('/api/data/:id', (req, res) => {
-    const result = findById(req.params.id, data);
+app.get('/api/notes/:id', (req, res) => {
+    const result = findById(req.params.id, notes);
     if (result){
      res.json(result);   
     } else{
@@ -70,15 +71,31 @@ app.get('/api/data/:id', (req, res) => {
     }
   });
 
-app.post('/api/data', (req, res) => {
+app.delete('/api/notes/:id', (req, res) => {
+    
+})
+
+app.post('/api/notes', (req, res) => {
     req.body.id = uuid();
     if (!validateNote(req.body)) {
         res.status(400).send('This note is not properly formatted.');
       } else {
-        const newNote = createNewNote(req.body, data);
+        const newNote = createNewNote(req.body, notes);
         res.json(newNote);
       }
 })
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+  });
+
+app.get('/notes', (req, res) =>{
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+  });
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
