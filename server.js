e9953701d81c8ab05170622cc6a1baf409f6918e
Amanda Klenk <amandamklenk3@gmail.com>
2022-06-23 13:22:24 -0400

    wrote functions to handle and validate new submissions, created app.port to handle them

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
+//requires
 const express = require('express');
 const data = require('./db/db.json');
+const fs = require('fs');
+const path = require('path');
 
+//express and port variables
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+// Helper method for generating unique ids
+const uuid = require('./helpers/uuid');
+
+//middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 function filterByQuery(query, data) {
     let filteredResults = data;
@@ -24,8 +34,51 @@ app.get('/api/data', (req, res) => {
     res.json(results);
   });
 
+function findById(id, data) {
+    const result = data.filter(note => note.id === id)[0];
+    return result;
+  }
 
+function createNewNote(body, data) {
+    const note = body;
+    data.push(note);
 
+    fs.writeFileSync(
+        path.join(__dirname, './db/db.json'),
+        JSON.stringify(data, null, 2)
+      );
+
+  return note;
+  }
+
+  function validateNote(note) {
+    if (!note.title || typeof note.title !== 'string') {
+      return false;
+    }
+    if (!note.text || typeof note.text !== 'string') {
+      return false;
+    }
+    return true;
+  }
+
+app.get('/api/data/:id', (req, res) => {
+    const result = findById(req.params.id, data);
+    if (result){
+     res.json(result);   
+    } else{
+        res.send(404);
+    }
+  });
+
+app.post('/api/data', (req, res) => {
+    req.body.id = uuid();
+    if (!validateNote(req.body)) {
+        res.status(400).send('This note is not properly formatted.');
+      } else {
+        const newNote = createNewNote(req.body, data);
+        res.json(newNote);
+      }
+})
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
