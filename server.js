08dc31885547af73f89781b24f366b52c4261b11
Amanda Klenk <amandamklenk3@gmail.com>
2022-06-23 12:53:28 -0400

    added Heroku

--- server.js
+++ server.js
@@ -1,11 +1,29 @@
 const express = require('express');
-const { data } = require('./db/db.json');
+const data = require('./db/db.json');
 
 const app = express();
 
+
+function filterByQuery(query, data) {
+    let filteredResults = data;
+    if (query.title) {
+      filteredResults = filteredResults.filter(note => note.title === query.title);
+    }
+    if (query.text) {
+      filteredResults = filteredResults.filter(note => note.text === query.text);
+    }
+    return filteredResults;
+  }
+
 app.get('/api/data', (req, res) => {
-    res.send('Hello!');
+    let results = data;
+    if(req.query){
+        results = filterByQuery(req.query, results);
+    }
+    res.json(results);
   });
+
+
 
 
 app.listen(3001, () => {
