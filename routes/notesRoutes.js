75b6d0cefdfddef8dcd027bd27ee1ce2756babc1
Amanda Klenk <amandamklenk3@gmail.com>
2022-06-25 12:14:19 -0400

    set up api router file in routes/apiRoutes folders

---
+++ routes/notesRoutes.js
@@ -0,0 +1,52 @@
+//requires
+var notes = require('../db/db.json');
+const fs = require('fs');
+const {  filterByQuery,
+    findById,
+    createNewNote,
+    validateNote } = require('../lib/notes');
+const router = require('express').Router();
+
+router.get('/notes', (req, res) => {
+    let results = notes;
+    if(req.query){
+        results = filterByQuery(req.query, results);
+    }
+    res.json(results);
+  });
+
+router.get('/notes/:id', (req, res) => {
+    const result = findById(req.params.id, notes);
+    if (result){
+     res.json(result);   
+    } else{
+        res.send(404);
+    }
+  });
+
+router.delete('/notes/:id', (req, res) => {
+    const result = findById(req.params.id, notes);
+    // console.log(req.params.id);
+    if (result){
+    var oldNotes = notes;
+    let filteredList = oldNotes.filter((note) => note.id !== req.params.id);
+    console.log(filteredList);
+
+fs.writeFileSync('../db/db.json', JSON.stringify(filteredList));
+    notes = filteredList; 
+    res.json(filteredList);
+    }
+  });
+
+router.post('/api/notes', (req, res) => {
+    req.body.id = uuid();
+    if (!validateNote(req.body)) {
+        res.status(400).send('This note is not properly formatted.');
+      } else {
+        const newNote = createNewNote(req.body, notes);
+        res.json(newNote);
+      }
+});
+
+//export
+module.exports = router;
--- server.js
+++ server.js
@@ -1,12 +1,7 @@
 //requires
 const express = require('express');
-var notes = require('./db/db.json');
-const fs = require('fs');
 const path = require('path');
-const {  filterByQuery,
-  findById,
-  createNewNote,
-  validateNote } = require('./lib/notes');
+
 
 //express and port variables
 const app = express();
@@ -22,46 +17,7 @@ app.use(express.static('public'));
 
 //app methods
 //api
-app.get('/api/notes', (req, res) => {
-    let results = notes;
-    if(req.query){
-        results = filterByQuery(req.query, results);
-    }
-    res.json(results);
-  });
 
-app.get('/api/notes/:id', (req, res) => {
-    const result = findById(req.params.id, notes);
-    if (result){
-     res.json(result);   
-    } else{
-        res.send(404);
-    }
-  });
-
-app.delete('/api/notes/:id', (req, res) => {
-    const result = findById(req.params.id, notes);
-    // console.log(req.params.id);
-    if (result){
-    var oldNotes = notes;
-    let filteredList = oldNotes.filter((note) => note.id !== req.params.id);
-    console.log(filteredList);
-
-fs.writeFileSync('./db/db.json', JSON.stringify(filteredList));
-    notes = filteredList; 
-    res.json(filteredList);
-    }
-  });
-
-app.post('/api/notes', (req, res) => {
-    req.body.id = uuid();
-    if (!validateNote(req.body)) {
-        res.status(400).send('This note is not properly formatted.');
-      } else {
-        const newNote = createNewNote(req.body, notes);
-        res.json(newNote);
-      }
-})
 //html
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'));
