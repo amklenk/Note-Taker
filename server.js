334f84f08eb584192012ba6766fd9a0baecea6f0
Amanda Klenk <amandamklenk3@gmail.com>
2022-06-25 13:25:48 -0400

    moved html routes back to server

--- server.js
+++ server.js
@@ -12,8 +12,20 @@ const htmlRoutes = require('./routes/htmlRoutes');
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use("/api", apiRoutes);
-app.use(htmlRoutes);
+// app.use(htmlRoutes);
 app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/notes', (req, res) =>{
+  res.sendFile(path.join(__dirname, './public/notes.html'));
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
+});
 
 //listen on heroku or localhost3001
 app.listen(PORT, () => {
